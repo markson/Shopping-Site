991748bd77aec89889cfeda9b9458092d3162754
mko_io <me@mko.io>
2014-08-01 01:54:34 +1200

    asyc works

--- app/controllers/products/index.js
+++ app/controllers/products/index.js
@@ -1,31 +1,23 @@
 export default Ember.ArrayController.extend({
 	actions: {
 		generateInvoice: function() {
-			var cup_1_amount;
-			var cup_2_amount;
-			var cup_3_amount;
-			this.store.find('product', 1).then(function(obj){
-				cup_1_amount = obj.get('amount');
-			});
-			this.store.find('product', 2).then(function(obj){
-				cup_1_amount = obj.get('amount');
-			});
-			this.store.find('product', 3).then(function(obj){
-				cup_1_amount = obj.get('amount');
-			});
-
-			$.post('http://nzud.co.nz/invoice',{
+			var store = this.store
+			store.find('product', 1).then(function(obj){
+				var cup_1_amount = obj.get('amount');
+				store.find('product', 2).then(function(obj){
+				var cup_2_amount = obj.get('amount');
+				store.find('product', 3).then(function(obj){
+				var cup_3_amount = obj.get('amount');
+				$.post('http://nzud.co.nz/invoice',{
 					cup_1: cup_1_amount,
 					cup_2: cup_2_amount,
-					cup_3:cup_3_amount
+					cup_3: cup_3_amount
 					} ).done(function(data){
 						alert("老余 the invoice has sent to your email");
-					})
-			// this.store.createRecord('product', {
-			// 	name:'Gulp',
-			// 	description:'Huge',
-			// 	amount:1
-			// });
+					});
+			});
+			});
+		});
 		}
 	}
 });
